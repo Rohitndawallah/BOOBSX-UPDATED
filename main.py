8c3c84700c8c508e1de837eaf2291a83d485fd99
JEEx07 <141727899+Rohitndawallah@users.noreply.github.com>
2025-07-09 14:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -561,14 +561,9 @@ async def upload(bot: Client, m: Message):
     raw_text6 = input6.text
     await input6.delete(True)
     await editable.delete()
-
-    #thumb = input6.text
-    #if thumb.startswith("http://") or thumb.startswith("https://"):
-        #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        #thumb = "thumb.jpg"
     else:
-        thumb == "no"
-    failed_count =0
+       thumb == "no"
+       failed_count =0
     if len(links) == 1:
         count = 1
     else:
